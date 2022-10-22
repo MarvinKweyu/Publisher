74d985ccd363ed206c70310630061d41fef444d3
Marvin Kweyu <mkweyu1@gmail.com>
2022-10-22 10:55:36 +0300

    chore: list posts overview

--- blog/models.py
+++ blog/models.py
@@ -3,21 +3,39 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
 
 class Post(models.Model):
     STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'))
     title = models.CharField(max_length=250)
+    # only one post with a slug for a given date
     slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()  # default manager
+    published = PublishedManager()  # custom manager
 
     class Meta:
         # recent posts appear first
         ordering = ('-publish',)
+        # Todo: come back to see how
+        # default_manager_name = PublishedManager
 
         def __str__(self):
             return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
