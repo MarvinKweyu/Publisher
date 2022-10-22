8981d645b7e3bed92a37a815609e66af513be8ad
Marvin Kweyu <mkweyu1@gmail.com>
2022-10-22 11:34:32 +0300

    chore: change post list to a class-based view

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,10 @@ from blog import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+
+    # path('', views.post_list, name='post_list'),  # using a function
+    path('', views.PostListView.as_view(),
+         name='post_list'),  # using a class
     path(
         '<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 from blog.models import Post
 
 # Create your views here.
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    # alternative syntax to get all posts instead of just published posts
+    # model = Post
+    queryset = Post.published.all()
+    # default without specification is object_list
+    # ? is this equivalent to post_list
+    context_object_name = 'posts'
+    paginate_by = 3
+    # default without specification is blog/post_list.html
+    template_name = 'blog/post/list.html'
+
+
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)  # 3 posts per paeg
+#     page = request.GET.get('page')
+
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # deliver the first page if hte page is not an integer
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # give the last page if the page is out of bounds
+#         posts = paginator.page(paginator.num_pages)
+
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
