ad6a31fb6476616e18dc6e5c29750ff2518b531a
Marvin Kweyu <mkweyu1@gmail.com>
2022-10-25 13:18:17 +0300

    feat: create custom template tags for most commented items and most recent articles

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,40 @@
+from django import template
+from django.db.models import Count
+from blog.models import Post
+
+
+register = template.Library()
+
+
+#  note diff with inclusinon_tag which processes the data and returns a rendered template
+# * optionally pass in name simple_tag(name="my_name") to override function name and use that as template tag name
+@register.simple_tag
+def total_posts():
+    """Count total number of published posts"""
+
+    return Post.published.count()
+
+
+@register.inclusion_tag("blog/post/latest_posts.html")
+def show_latest_posts(count: int = 5):
+    """Show the latests posts ordered by publish date"""
+    latest_posts = Post.published.order_by("-publish")[:count]
+    return {"latest_posts": latest_posts}
+
+
+@register.inclusion_tag("blog/post/most_commented.html")
+def show_most_commented_posts(count: int = 5, comment_count: int = 2):
+    """Show posts with most comments"""
+    most_commented = Post.published.filter(comments__gt=comment_count).order_by(
+        "-publish"
+    )[:count]
+    return {"most_commented": most_commented}
+
+
+#  diff impl
+# better approach
+@register.simple_tag
+def get_most_commented(count: int = 5):
+    return Post.published.annotate(total_comments=Count("comments")).order_by(
+        "-total_comments"
+    )[:count]
